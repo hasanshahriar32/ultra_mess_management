51725825f2e92dc052cd4748c4b0278e4f074bba
Mr. Hasan <hasanshahriar32@gmail.com>
2023-01-21 04:12:22 +0600

    meal schedule updated

--- components/heroDateSelect/dateSelect.js
+++ components/heroDateSelect/dateSelect.js
@@ -8,10 +8,40 @@ import "./dateselect.module.css";
 const DateSelect = () => {
   let calendarInstance;
   const selectedValues = [];
+  const [modmindate, setmodMindate] = React.useState(new Date());
+  const [modmaxdate, setmodMaxdate] = React.useState(new Date());
+
+  //if mealDate is available
+  React.useEffect(() => {
+    const meal = localStorage.getItem("mealDate");
+    const mealDates = meal.split(" ");
+    const mealDate1 = mealDates[0];
+    const mealDate2 = mealDates[1];
+    setmodMindate(mealDate1);
+    setmodMaxdate(mealDate2);
+  }, []);
+
+  // set min and max date
+  const updateMinSection = (e) => {
+    const minDateValue = new Date(document.getElementById("minDate").value);
+    // calendarInstance.min = minDateValue;
+    // calendarInstance.dataBind();
+    console.log(e.target.value);
+    setmodMindate(e.target.value);
+    // setMindate(minDate);
+  };
+  const updateMaxSection = (e) => {
+    const minDateValue = new Date(document.getElementById("minDate").value);
+    // calendarInstance.min = minDateValue;
+    // calendarInstance.dataBind();
+    console.log(e.target.value);
+    setmodMaxdate(e.target.value);
+    // setMindate(minDate);
+  };
 
   // range
-  const minDate = new Date(new Date().getFullYear(), new Date().getMonth(), 7);
-  const maxDate = new Date(new Date().getFullYear(), new Date().getMonth(), 27);
+  const minDate = modmindate;
+  const maxDate = modmaxdate;
 
   function onchange(args) {
     /*Displays selected date in the label*/
@@ -35,7 +65,9 @@ const DateSelect = () => {
       );
     }
   }
-
+  const setSubmit = () => {
+    localStorage.setItem("mealDate", modmindate + " " + modmaxdate);
+  };
   return (
     <div
       className="hero min-h-screen bg-base-200"
@@ -43,6 +75,7 @@ const DateSelect = () => {
     >
       <div className="hero-content flex-col lg:flex-row gap-5 ">
         <div className=" control-section bg-base-300 m-3 p-4 rounded-lg">
+          {/* control  */}
           <div
             id="control_wrapper"
             className="col-lg-6 col-sm-8 col-md-8 multiselectWrapper "
@@ -63,7 +96,40 @@ const DateSelect = () => {
             </div>
           </div>
         </div>
+
         <div>
+          {/* min and max  */}
+          <div className="my-3">
+            <label style={{ paddingTop: "22px" }}>From Date </label>
+            <input
+              type="date"
+              id="minDate"
+              onChange={updateMinSection}
+              className="e-input mr-5"
+            />
+            <label style={{ paddingTop: "22px" }}>To Date </label>
+            <input
+              type="date"
+              id="maxDate"
+              className="e-input"
+              onChange={updateMaxSection}
+              //set value 7 days from today
+              // defaultValue={
+              //   new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000)
+              //     .toISOString()
+              //     .split("T")[0]
+              // }
+            />
+            <button
+              type="submit"
+              className="btn btn-secondary btn-outline mx-2"
+              onClick={setSubmit}
+            >
+              Set
+            </button>
+          </div>
+
+          {/* selected dates  */}
           <label style={{ paddingTop: "22px" }}>Selected Meal Dates</label>
           <div className="content-value">
             <div id="multiselect"></div>
